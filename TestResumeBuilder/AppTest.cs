5c17d07c228b0015a2a65f850d500a0b22b54bfe
Christofer Cousins <77406318+csc530@users.noreply.github.com>
2023-09-17 11:22:03 -0400

    :white_check_mark: refac: change tests file structure
    move each test of a command to a separate class and file for better organization

--- TestResumeBuilder/AppTest.cs
+++ TestResumeBuilder/AppTest.cs
@@ -1,37 +1,17 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Spectre.Console.Testing;
+﻿using Spectre.Console.Testing;
 
 namespace TestResumeBuilder
 {
-	public class Tests
+	public class AppTest
 	{
-		resume_builder.App cliapp;
-		TestConsole console;
+		internal resume_builder.App cliapp;
+		internal TestConsole console;
 
 		[SetUp]
 		public void Setup()
 		{
 			console = new TestConsole();
 			cliapp = new resume_builder.App(console);
-		}
-
-		[Test]
-		public void AddJob_WithNoArguments_ShouldFail()
-		{
-			Assert.Catch(() => cliapp.Run(new[] { "add", "job" }), "Expected to fail");
-		}
-
-		[Test]
-		public void AddJob_WithoutStartDate_ShouldFail([Random(100, Distinct = true)] string title)
-		{
-			var args = new string[] { "add", "job", "--title", title };
-			int exitcode = cliapp.Run(args);
-			Assume.That(title.Length < 100 && title.Length > 0);
-			Assert.Multiple(() =>
-			{
-				Assert.That(exitcode, Is.EqualTo(0));
-				Assert.That(console.Output, Contains.Value(title));
-			});
 		}
 	}
 }
