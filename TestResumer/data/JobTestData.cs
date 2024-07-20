7387f6b0128edc4a43cada86e111bc1f736dc84f
smellilac <littleklyksa@gmail.com>
2024-07-20 12:21:16 +0300

    Removed OrNull()

--- TestResumer/data/JobTestData.cs
+++ TestResumer/data/JobTestData.cs
@@ -7,8 +7,8 @@ internal class JobTestData: TestData
 {
     private static Faker<Job> BogusJob { get; set; } = new Faker<Job>()
                                                       .RuleFor(job => job.Title, RandomJobTitle)
-                                                      .RuleFor(job => job.Description, RandomListOfTextOrNull)
-                                                      .RuleFor(job => job.Experience, RandomListOfTextOrNull)
+                                                      .RuleFor(job => job.Description, RandomListOfText)
+                                                      .RuleFor(job => job.Experience, RandomListOfText)
                                                       .RuleFor(job => job.Company, RandomCompany)
                                                       .RuleFor(job => job.StartDate, RandomPastDate)
                                                       .RuleFor(job => job.EndDate, RandomEndDate);
